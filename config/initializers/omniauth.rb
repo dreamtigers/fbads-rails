6eedc0a8c3c077c909fa7ac39687e9351890bae9
Ivan Gonzalez Polanco <ivan14polanco@gmail.com>
2020-05-01 19:13:34 -0400

    Add permissions scope to omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,7 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, Rails.application.credentials.fb_app[:id], Rails.application.credentials.fb_app[:secret]
+  provider :facebook, Rails.application.credentials.fb_app[:id],
+    Rails.application.credentials.fb_app[:secret],
+    scope: 'manage_pages,ads_management'
 end
