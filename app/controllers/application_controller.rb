71b3cfec634436837705068691d678e12b63a69e
Ivan Gonzalez Polanco <ivan14polanco@gmail.com>
2020-05-02 09:54:51 -0400

    Add filter 'require_login' to view pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,13 @@ class ApplicationController < ActionController::Base
     @current_user ||= FbUser.find(session[:user_id]) if session[:user_id]
   end
   helper_method :current_user
+
+  def require_login
+    # If we're logged in, simply return
+    return if current_user
+
+    # Otherwise redirect to '/'
+    redirect_to root_url, alert: 'You must be logged in to access this section'
+  end
+  helper_method :require_login
 end
