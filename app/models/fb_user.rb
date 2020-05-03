694ee78795a1fa23c8e8d4d855a8ab4a617fe84d
Ivan Gonzalez Polanco <ivan14polanco@gmail.com>
2020-05-03 16:34:27 -0400

    Replace the way adaccount id and page id are fetched

--- app/models/fb_user.rb
+++ app/models/fb_user.rb
@@ -1,9 +1,7 @@
 class FbUser < ApplicationRecord
 
-  # When you find a FbUser, call the ad_acct_query setter to instantiate an
-  # ad_account.
-  after_find :ad_acct_query=
   after_find :fb_session=
+  after_find :ad_acct_query=
 
   # A class method uses self to distinguish from instance methods.
   # It can only be called on the class, not an instance.
@@ -14,15 +12,11 @@ class FbUser < ApplicationRecord
       user.email = auth.info.email
       user.token = auth.credentials.token
 
-      # user_query = FacebookAds::User.get(id, @session)
-      # ad_account_info
-      # Get the ad account info of our user
-      ad_account_info = graph_get(fb_query('me/adaccounts?fields=account_id,account_status', user.token))
-      # So we can save the ad account id
-      user.adaccount = ad_account_info['data'][0]['id']
+      session = FacebookAds::Session.new(access_token: user.token)
+      user_query = FacebookAds::User.get(uid, session)
+      user.adaccount = user_query.adaccounts.first.account_id
+      user.pageID = user_query.accounts.first.id
 
-      pages_info = graph_get(fb_query('me/accounts?limit=100', user.token))
-      user.pageID = pages_info['data'][0]['id']
       # user.url =
       user.active = 1
       user.save!
@@ -33,7 +27,8 @@ class FbUser < ApplicationRecord
   def ad_acct_query=
     # The app_secret and api version are already set in the initializer
     # We set up the session per user, that's the reason for this method.
-    @ad_acct_query = FacebookAds::AdAccount.get(self.adaccount, 'name', @session)
+    session = FacebookAds::Session.new(access_token: self.token)
+    @ad_acct_query = FacebookAds::AdAccount.get(self.adaccount, 'name', session)
   end
   # Getter
   attr_reader :ad_acct_query
@@ -45,13 +40,4 @@ class FbUser < ApplicationRecord
   # Getter
   attr_reader :fb_session
 
-  private
-
-  def self.fb_query(uri, token)
-    return URI("https://graph.facebook.com/v6.0/#{uri}&access_token=#{token}")
-  end
-
-  def self.graph_get(query)
-    JSON.parse(Net::HTTP.get(query))
-  end
 end
