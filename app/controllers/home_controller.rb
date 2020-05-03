efd075637565eae73c6362b816eb4ec0cfd4ad75
Ivan Gonzalez Polanco <ivan14polanco@gmail.com>
2020-05-03 17:47:05 -0400

    Add settings management

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,36 @@
 class HomeController < ApplicationController
+  before_action :require_login, only: [:edit, :update]
+  before_action :set_fb_user, only: [:edit, :update]
+
   def index
   end
+
+  # GET /edit
+  def edit
+    user_query = FacebookAds::User.get(@fb_user.uid, @fb_user.fb_session)
+    @ad_accounts = user_query.adaccounts(fields: 'name,account_id').map {|a| [a.name, a.account_id]}
+    @pages = user_query.accounts(fields: 'name,id').map {|p| [p.name, p.id]}
+  end
+
+  # PATCH/PUT /edit
+  def update
+    respond_to do |format|
+      if @fb_user.update(fb_user_params)
+        format.html { redirect_to root_path, notice: 'User was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+  private
+
+  def set_fb_user
+    @fb_user = FbUser.find(session[:user_id])
+  end
+
+  def fb_user_params
+    params.require(:fb_user).permit(:adaccount, :pageID, :url)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'ads/index'
-  post 'ads/create'
-  get 'ads/new'
+  resources :ads, only: [:index, :create, :new]
+  resources :settings, only: [:edit, :update], controller: 'home'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   match 'auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
   match 'auth/failure', to: redirect('/'), via: [:get, :post]
