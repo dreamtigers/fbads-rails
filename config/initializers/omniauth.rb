7028ea4217dc2511e3cbf5059edc7ab727bbe581
Ivan Gonzalez Polanco <ivan14polanco@gmail.com>
2020-05-11 12:14:25 -0400

    Make OAuth use API v6.0

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,5 +3,9 @@ OmniAuth.config.logger = Rails.logger
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, Rails.application.credentials.fb_app[:id],
            Rails.application.credentials.fb_app[:secret],
-           scope: 'email,manage_pages,ads_management'
+           scope: 'email,manage_pages,ads_management',
+           client_options: {
+             site: 'https://graph.facebook.com/v6.0',
+             authorize_url: "https://www.facebook.com/v6.0/dialog/oauth"
+           }
 end
