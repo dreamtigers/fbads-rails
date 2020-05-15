c0c86d6d3e74302614e4d4e4114d93a4af74f2c7
Ivan Gonzalez Polanco <ivan14polanco@gmail.com>
2020-05-15 13:47:07 -0400

    Replace credentials for env vars

--- config/initializers/facebookbusiness.rb
+++ config/initializers/facebookbusiness.rb
@@ -1,6 +1,6 @@
 FacebookAds.configure do |config|
   config.api_version = 'v6.0'
-  config.app_secret = Rails.application.credentials.fb_app[:secret]
+  config.app_secret = ENV['FACEBOOK_APP_SECRET']
   # Log HTTP request & response to logger
   config.log_api_bodies = true
   # Logger for debugger
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,8 +1,9 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, Rails.application.credentials.fb_app[:id],
-           Rails.application.credentials.fb_app[:secret],
+  # provider :facebook, Rails.application.credentials.fb_app[:id],
+  #          Rails.application.credentials.fb_app[:secret],
+  provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET'],
            scope: 'email,manage_pages,ads_management',
            client_options: {
              site: 'https://graph.facebook.com/v6.0',
