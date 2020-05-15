33a57030bd3cd0e7632f9d7ae5756fc4fa69be51
Ivan Gonzalez Polanco <ivan14polanco@gmail.com>
2020-05-15 00:15:39 -0400

    Improve fb_ads validations

--- app/models/fb_ad.rb
+++ app/models/fb_ad.rb
@@ -7,7 +7,20 @@ class FbAd < ApplicationRecord
   validates :video_url, presence: true
   validates :thumbnail_url, presence: true
   validates :pixel_id, presence: true
-  validates :interests, presence: true, length: { minimum: 4, too_short: ": select at least one." }
-  validates :countries, presence: true, length: { minimum: 4, too_short: ": select at least one." }
-  # validates :countries, presence: true, unless: -> { (JSON.parse countries).empty? }, message: "Select at least one"
+  # validates :interests, presence: true, length: { minimum: 4, too_short: ": select at least one." }
+  # validates :countries, presence: true, length: { minimum: 4, too_short: ": select at least one." }
+  validate :at_least_one_interest, :at_least_one_country
+
+  def at_least_one_country
+    if (JSON.parse countries).empty?
+      errors.add(:countries, "must be one or more")
+    end
+  end
+
+  def at_least_one_interest
+    if (JSON.parse interests).empty?
+      errors.add(:interests, "must be one or more")
+    end
+  end
+
 end
